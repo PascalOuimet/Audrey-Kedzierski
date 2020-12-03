e9625ae119cb5b113f0fecb8a5cf2f5bd3326267
Ouimet <Pascal.Ouimet@astenjohnson.com>
2020-12-02 20:31:48 -0500

    success page back to english version

--- src/pages/success.js
+++ src/pages/success.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 const Success = () => (
     <>
         <p>I received your message.<br />I will contact you shortly.</p>
-        <Link to="/">Back to the homepage</Link>
+        <Link to="/en">Back to the homepage</Link>
     </>
 )
 

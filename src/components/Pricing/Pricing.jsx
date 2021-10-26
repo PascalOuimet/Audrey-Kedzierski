4dd39386103bb9d4c0ef1a40a71d4e026ec51429
ouimepa <ouimepa@gmail.com>
2021-10-26 19:38:44 -0400

    linting fix

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -4,14 +4,14 @@ import { Container } from 'react-bootstrap';
 import Title from '../Title/Title';
 
 const Pricing = ({data}) => {
-    const { title, individual_1, individual_2, couple_1, couple_2, receipt, star, star2 } = data;
+    const { title, individual1, individual2, couple1, couple2, receipt, star, star2 } = data;
 
     return (
         <Fade bottom duration={1000} delay={300} distance="0px">
             <section id="pricing">
                 <Container>
                     <Title title={title || ""} />
-                    <p><strong>{individual_1} </strong>{individual_2}<br /><strong>{couple_1} </strong>{couple_2}</p>
+                    <p><strong>{individual1} </strong>{individual2}<br /><strong>{couple1} </strong>{couple2}</p>
                     <p>{receipt}</p>
                     <p className="price-note">{star}</p>
                     <p className="price-note">{star2}</p>
